7e39e43218d50422166a40fb17ff1682e708745f
Godina <godina.n.2019@gmail.com>
2021-03-18 18:50:58 +0000

    Removed Jumpbox VM

--- main.tf
+++ main.tf
@@ -33,27 +33,10 @@ module "web-vm" {
   }
 }
 
-module "jumpbox-vm" {
-  source = "./modules/vm-linux-latest"
-  vm_name = "jumpbox-vm"
-  location = var.location
-  prefix = var.prefix
-  resource_group_name = module.fs_hub_vnet.vnet_rg 
-  subnet_id           = module.fs_hub_vnet.subnet_id[1]
-  tags = {
-    Environment = var.prefix
-    function    = "JUMPBOX VM"
-  }
-}
-
 output "dbvm_pip" {
  value = module.db-vm.public_ip
 }
 
 output "webvm_pip" {
  value = module.web-vm.public_ip
-}
-
-output "jumpvm_pip" {
- value = module.jumpbox-vm.public_ip
 }
